0ee54dd13fd1ad40ed41c0c2f6c11e2ebfd872f6
Timothe <timothe.rousseau1@gmail.com>
2021-05-25 10:49:22 -0500

    confidentialite

--- pages/confidentialite.vue
+++ pages/confidentialite.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="container__section">
-    <h1>Politique de confidentialite</h1>
+    <h1 class="page__title">Mentions Legales</h1>
+    <div
+      class="page__content"
+      v-if="confidentialite.content"
+      v-html="$md.render(confidentialite.content)"
+    ></div>
   </div>
 </template>
 
 <script>
-  export default {};
+  export default {
+    async asyncData({ $strapi }) {
+      return {
+        confidentialite: await $strapi.find("confidentialite"),
+      };
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
